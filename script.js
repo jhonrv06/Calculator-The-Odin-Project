c801cc063a64d282bbe15c108fbff2a014a7dd7d
Jhon <19398401+jhonrv06@users.noreply.github.com>
2025-11-24 21:49:02 -0500

    Se modifica funcion calculateValue
    Se modifica debido a que no devuelve los valores
    correctos al momento de ingresar numeros de dos
    digitos.

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ function validateOperation(value1,sing,value2){
 
 
 function saveValue(value){
-    let resultArray = values.push(value)
-    console.log(values)
+    if(value != "="){
+        values.push(value)
+    }
 }
 
 function validateSings(data){
@@ -26,31 +27,72 @@ function validateSings(data){
     }else if(data == "="){
         return true
     }
-    console.log(operator)
+   
    return (operator == 2) ? true: false
 }
 
 function generateCalculation(data){
     
     if(data){
-        calculateValue(values)
+        
+        let value = calculateValue(values)
+        calculateValue(values);
+        removSpans();
+        displayOnScreem(calculateValue(values))
+        resetData()
+        saveValue(value)
+        //devolver values a 0
     }
-    console.log(data)
+   
 }
 
 
 
 function calculateValue(objet){
+    
+
+    objet.forEach(element => {
+
+        //console.log(Number(element))
+        
+        if(Number.isNaN(Number(element))){
+            
+            console.log(element)
+            
+        }
+    })
+
+    
+    /*
     let longArray = objet.length;
 
     let value1 = parseInt(objet.join(""));
     let evaluate = value1.toString().length
     let cutvalue2 = objet.slice(evaluate+1, longArray);
+    //ejecitar un metodo qu eme traiga la posicion del signo
+    //debo modificar el algoritmo ya que cuando el valor ingredo en el array es de dos digitos este me hace el objer.slice como si hubieran dos numeros en ela rray separados que se unieron
     let value2 = parseInt(cutvalue2.join(""));
     let sing = objet.slice(evaluate, evaluate+1);
-    
-    console.log(validateOperation(value1,sing[0],value2)) 
 
+    console.log("value1 es " + value1)
+    console.log("value2 es " + value2)*/
+
+    //return validateOperation(value1,sing[0],value2)
+
+}
+
+function resetData(){
+    values = [];
+    operator = 0;
+}
+
+function removSpans(){
+    const SPANS = document.querySelectorAll("span");
+
+    SPANS.forEach(element => {
+        element.remove();
+    });
+    
 }
 
 
@@ -64,8 +106,6 @@ function displayOnScreem(data){
 function getEvents(element){
     element.addEventListener("click", (e) =>{
             const VALUEELEMENT = e.target.value;
-
-            //aqui debo llamar una funcion que me evalue si ya hay un sigo cuando el valor recibido sea un signo
             saveValue(VALUEELEMENT)
             displayOnScreem(VALUEELEMENT)
             generateCalculation(validateSings(VALUEELEMENT))
